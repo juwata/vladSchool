d0ba072808f998eb6dbbc61df9ff65d0db453f9f
Júlia Fonseca Watanabe <juliawatanabe-ieg@instituto.ad>
2026-02-26 11:06:08 -0300

    popup add estudante

---
+++ scripts/adm.js
@@ -0,0 +1,9 @@
+
+// popup editar x excluir
+const popup = document.querySelector('#whatToDo');
+
+document.querySelectorAll('#alunos tbody tr')
+    .forEach(tr => tr.addEventListener('click', () => {
+        popup.querySelector('')
+
+    }))
